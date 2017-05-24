9c6641838bd802b77606a0feae04e6bb8c92e604
Kerna96 <daniel.coughran@hotmail.com>
2017-05-24 14:43:14 +0800

    Some scene updates and various scripts created
    ADDED: ConversationDisplay.cs
    ADDED: ConversationManager.cs
    ADDED: VRFacade.cs
    UPDATED: .gitignore now based off github .gitignore unity template
    CHANGED: unityiiiscenes now has a conversation UI canvas, all
    conversation elements will be added here
    CHANGED: Some test UI elements with attached scripts added to
    conversation canvas
    CHANGED: Player game object now contains both the VR and main cameras

---
+++ ICT371_Main_Project/Assets/ConversationDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ICT371 {
+    public class ConversationDisplay : MonoBehaviour {
+        public Text m_textDisplay;
+
+        // Use this for initialization
+        void Start() {
+        }
+
+        // Update is called once per frame
+        void Update() {
+        }
+
+        public void ChangeDisplayedText(string changeTo) {
+            m_textDisplay.text = changeTo;
+        }
+    }
+}
---
+++ ICT371_Main_Project/Assets/ConversationManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ICT371 {
+    public class ConversationManager : MonoBehaviour {
+
+        private bool m_textChanged = true;
+        private ConversationDisplay m_textConvoDisplay;
+        private string m_changedText;
+        public Text m_textDisplay;
+
+        // Use this for initialization
+        void Start() {
+            m_textConvoDisplay = m_textDisplay.GetComponent<ConversationDisplay>();
+            //TODO Register any other components
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if(m_textChanged) {
+                m_textConvoDisplay.ChangeDisplayedText(m_changedText);
+            }
+        }
+    }
+}
