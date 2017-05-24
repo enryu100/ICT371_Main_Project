7cf73fcaea9cbfe3dde0b75e94a6a0f7873fe227
Kerna96 <daniel.coughran@hotmail.com>
2017-05-25 00:52:22 +0800

    Several Imports and some small scene changes
    Imported: Standard Assets character stuff
    Imported: Two small texture packs, only some textures kept
    Changed: Scripts moved to sub directory under ICT371

---
+++ ICT371_Main_Project/Assets/ICT371/Scripts/VRFacade.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ICT371
+{
+    public class VRFacade : MonoBehaviour
+    {
+
+        public GameObject m_mainCam;
+        public GameObject m_VRCam;
+        public GameObject m_VRManager;
+        public bool m_VRMode;
+        
+        // Checks connection of the camera after each frame.
+        private void SetCameras()
+        {
+            if(!UnityEngine.VR.VRDevice.isPresent) {
+                m_VRMode = false;
+            }
+
+            if (m_VRMode)
+            {
+                m_mainCam.SetActive(false);
+                m_VRCam.SetActive(true);
+                m_VRManager.SetActive(true);
+            }
+            else
+            {
+                m_mainCam.SetActive(true);                
+                m_VRCam.SetActive(false);
+                m_VRManager.SetActive(false);
+            }
+        }
+        
+        // Use this for initialization
+        void Start()
+        {
+            SetCameras();
+        }
+
+        //Fixed update to lower performance drain as opposed to Update()
+        private void FixedUpdate()
+        {
+            SetCameras();
+        }
+    }
+}
